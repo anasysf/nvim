5d594f4bc726b066a7bfba31c1df6e9492061f87
anasysf <anayoussef.elmahdad@gmail.com>
2023-12-21 23:54:59 +0100

    changed lualine config

--- lua/xbogayo/lazy/plugins/lualine.lua
+++ lua/xbogayo/lazy/plugins/lualine.lua
@@ -4,15 +4,6 @@ return {
     options = {
       theme = 'catppuccin',
     },
-    sections = {
-      lualine_x = {
-        {
-          require('lazy.status').updates,
-          cond = require('lazy.status').has_updates,
-          color = { fg = '#ff9e64' },
-        },
-      },
-    },
   },
   dependencies = 'nvim-tree/nvim-web-devicons',
 }
