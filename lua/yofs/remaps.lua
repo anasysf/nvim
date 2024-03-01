2e376b20dfc2360a3ad8f35b08397d3822a8a534
Anas Youssef El Mahdad <anayoussef.elmahdad@proton.me>
2024-03-01 17:45:49 +0100

    some remaps to visual mode

--- lua/yofs/remaps.lua
+++ lua/yofs/remaps.lua
@@ -1,7 +1,7 @@
 local keymap = vim.keymap
 local opts = { noremap = false, silent = true }
 
-keymap.set('n', '<A-+>', '<C-a>', opts)
-keymap.set('n', '<A-->', '<C-x>', opts)
+keymap.set({ 'n', 'v' }, '<A-+>', '<C-a>', opts)
+keymap.set({ 'n', 'v' }, '<C-x>', opts)
 
-keymap.set('n', '<C-a>', 'ggVG', opts)
+keymap.set({ 'n', 'v' }, '<C-a>', 'ggVG', opts)
